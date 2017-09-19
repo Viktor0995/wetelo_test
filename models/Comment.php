8447d1eedf54bf66b80b9eb89f77b1f011d061e8
Viktor Novchuk <vnovchuk@gmail.com>
2017-09-19 19:11:56 +0300

    Create admin module

---
+++ models/Comment.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comment".
+ *
+ * @property integer $id
+ * @property integer $post_id
+ * @property string $content
+ * @property integer $user_id
+ * @property string $date_created
+ * @property integer $status
+ *
+ * @property Post $post
+ * @property User $user
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'comment';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['post_id', 'user_id', 'status'], 'integer'],
+            [['content'], 'string'],
+            [['date_created'], 'safe'],
+            [['post_id'], 'exist', 'skipOnError' => true, 'targetClass' => Post::className(), 'targetAttribute' => ['post_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'post_id' => 'Post ID',
+            'content' => 'Content',
+            'user_id' => 'User ID',
+            'date_created' => 'Date Created',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPost()
+    {
+        return $this->hasOne(Post::className(), ['id' => 'post_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
---
+++ models/Post.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property integer $category_id
+ * @property integer $status
+ * @property string $pub_date
+ *
+ * @property Comment[] $comments
+ * @property Postimage[] $postimages
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content'], 'string'],
+            [['category_id', 'status'], 'integer'],
+            [['pub_date'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'category_id' => 'Category ID',
+            'status' => 'Status',
+            'pub_date' => 'Pub Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['post_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPostimages()
+    {
+        return $this->hasMany(Postimage::className(), ['post_id' => 'id']);
+    }
+}
